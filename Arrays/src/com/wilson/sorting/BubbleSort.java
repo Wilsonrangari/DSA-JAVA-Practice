a1423386ae0382064f5942bbbc367203385bea67
Wilsonrangari <rangariwilson@gmail.com>
2024-06-14 02:35:55 -0700

    BubbleSort code commit

---
+++ Arrays/src/com/wilson/sorting/BubbleSort.java
@@ -0,0 +1,47 @@
+package com.wilson.sorting;
+
+//Brute force Approach
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] arr={5,4,3,2,1};
+
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i] > arr[i+1]) {
+                //swap
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+        for(int i=0;i<arr.length-2;i++){
+            if(arr[i] > arr[i+1]) {
+                //swap
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+        for(int i=0;i<arr.length-3;i++){
+            if(arr[i] > arr[i+1]) {
+                //swap
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+        for(int i=0;i<arr.length-4;i++){
+            if(arr[i] > arr[i+1]) {
+                //swap
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+
+        }
+    }
+}
