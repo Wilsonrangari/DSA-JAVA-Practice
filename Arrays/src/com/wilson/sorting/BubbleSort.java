1c15def6f599c9d450c6f8ebd493669f29334d03
Wilsonrangari <rangariwilson@gmail.com>
2024-06-14 02:41:29 -0700

    BubbleSort code updated commit

--- Arrays/src/com/wilson/sorting/BubbleSort.java
+++ Arrays/src/com/wilson/sorting/BubbleSort.java
@@ -6,39 +6,52 @@ public class BubbleSort {
 
     public static void main(String[] args) {
         int[] arr={5,4,3,2,1};
+        for (int j = 0; j < arr.length; j++) {
+            for (int i = 0; i < arr.length-1-j; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    //swap
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
 
-        for(int i=0;i<arr.length-1;i++){
-            if(arr[i] > arr[i+1]) {
-                //swap
-                int temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
             }
         }
-        for(int i=0;i<arr.length-2;i++){
-            if(arr[i] > arr[i+1]) {
-                //swap
-                int temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
-            }
-        }
-        for(int i=0;i<arr.length-3;i++){
-            if(arr[i] > arr[i+1]) {
-                //swap
-                int temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
-            }
-        }
-        for(int i=0;i<arr.length-4;i++){
-            if(arr[i] > arr[i+1]) {
-                //swap
-                int temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
-            }
-        }
+
+
+
+//        for(int i=0;i<arr.length-1;i++){
+//            if(arr[i] > arr[i+1]) {
+//                //swap
+//                int temp = arr[i];
+//                arr[i] = arr[i+1];
+//                arr[i+1] = temp;
+//            }
+//        }
+//        for(int i=0;i<arr.length-2;i++){
+//            if(arr[i] > arr[i+1]) {
+//                //swap
+//                int temp = arr[i];
+//                arr[i] = arr[i+1];
+//                arr[i+1] = temp;
+//            }
+//        }
+//        for(int i=0;i<arr.length-3;i++){
+//            if(arr[i] > arr[i+1]) {
+//                //swap
+//                int temp = arr[i];
+//                arr[i] = arr[i+1];
+//                arr[i+1] = temp;
+//            }
+//        }
+//        for(int i=0;i<arr.length-4;i++){
+//            if(arr[i] > arr[i+1]) {
+//                //swap
+//                int temp = arr[i];
+//                arr[i] = arr[i+1];
+//                arr[i+1] = temp;
+//            }
+//        }
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
 
