00f57c2549bc3053ffbcd380432c7174d5eebf55
Wilsonrangari <rangariwilson@gmail.com>
2024-06-15 05:03:39 -0700

    LC.70 Opti_ClimbingStairs commit

---
+++ Arrays/src/com/wilson/DP/Optim_ClimbingStairs_70.java
@@ -0,0 +1,33 @@
+package com.wilson.DP;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Optim_ClimbingStairs_70 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Total ways to climb stair is "+totalWays(0, n, new HashMap<Integer, Integer>()));
+    }
+    private static int totalWays(int currentStair, int targetStair, HashMap<Integer, Integer> memo) {
+            if (targetStair == currentStair)
+                return 1;
+            if (targetStair < currentStair)
+                return 0;
+
+            int currentKey = currentStair;;
+            if(memo.containsKey(currentKey)){
+                return memo.get(currentKey);
+            }
+
+            int oneJump = totalWays(currentStair + 1, targetStair, memo);
+            int twoJump = totalWays(currentStair + 2, targetStair, memo);
+
+            memo.put(currentKey, oneJump + twoJump);
+            System.out.println(memo);
+
+            //return memo.get(currentKey);
+              return oneJump + twoJump;
+        }
+}
